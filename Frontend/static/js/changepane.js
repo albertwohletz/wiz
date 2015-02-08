735ef2f8789b69d5a9ba9415c04a1218acd928f0
Albert Wohletz <albert@wohletz.net>
2015-02-08 02:26:13 -0800

    base attributes and more core stuff for first tab

--- Frontend/static/js/changepane.js
+++ Frontend/static/js/changepane.js
@@ -1,3 +1,13 @@
 /**
  * Created by albertlwohletz on 2/7/15.
  */
+$(document).ready(function() {
+	$('.blah').click(function() {
+		alert('hi');
+	});
+	$( ".spinner" ).spinner({
+		min: 1,
+		max: 10,
+		start: 1
+	});
+});
--- Frontend/views.py
+++ Frontend/views.py
@@ -2,5 +2,19 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    params = {}
+    params = {
+    	'attributes': [
+    		{'short': 'bod', 'full': 'Body'}, 
+    		{'short': 'agi', 'full': 'Agility'}, 
+    		{'short': 'rea', 'full': 'Reaction'}, 
+    		{'short': 'str', 'full': 'Strength'}, 
+    		{'short': 'cha', 'full': 'Charisma'}, 
+    		{'short': 'int', 'full': 'Intuition'}, 
+    		{'short': 'log', 'full': 'Logic'}, 
+    		{'short': 'wil', 'full': 'Willpower'}, 
+    		{'short': 'edg', 'full': 'Edge'}, 
+    		{'short': 'mag', 'full': 'Magic'}, 
+    		{'short': 'res', 'full': 'Resonance'}
+    	]
+    }
     return render(request, 'main.html', params)
