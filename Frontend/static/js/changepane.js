91e50c16736ee04202bfb19d2bc479de3f48a3de
Albert Wohletz <albert@wohletz.net>
2015-02-07 18:38:51 -0800

    More basic structure

---
+++ Frontend/static/js/changepane.js
@@ -0,0 +1,3 @@
+/**
+ * Created by albertlwohletz on 2/7/15.
+ */
---
+++ Frontend/static/js/main.js
@@ -0,0 +1,8 @@
+/**
+ * Created by albertlwohletz on 2/7/15.
+ */
+
+// Init JQueryUI Elements
+$(function() {
+    $( ".resizable" ).resizable();
+});
