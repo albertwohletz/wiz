4e8d924b065dc4229e026d012d03940f3a0ff698
Albert Wohletz <albert@wohletz.net>
2015-02-08 04:50:53 -0800

    overlay hide/show with popup

--- Frontend/static/js/main.js
+++ Frontend/static/js/main.js
@@ -2,17 +2,28 @@
  * Created by albertlwohletz on 2/7/15.
  */
 
-// Init JQueryUI Elements
+// Init JQuery Elements
 $(function() {
     $( ".resizable" ).resizable();
     $( ".overlay" ).draggable();
-    // temp
-    $( ".overlay").show();
+
+    // Overlay close button
+    $('.close-button').click(function() {
+    	$(this).parent().hide();
+	});
+
+
 });
 
+// Qualities Functios
 $(function(){
+	$('.add-qualities').click(function(){
+		$('.positive-qualities').show();
+	});
 	$('.positive-quality-item').click(function(e){
 		desc = 'karma: ' + $( this ).val() + '</br>' + $( this ).attr('description')
 		$('.quality-description').html(desc);
 	});
 });
+
+// Closes Parent of who was clicked.  This way you can have this in a container and it will act as an close 'x'.
