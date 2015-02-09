6f98e8ff78d3d8a9383deae7838e7f5f5a682a59
Albert Wohletz <albert@wohletz.net>
2015-02-08 22:03:19 -0800

    magic priorities.

--- Frontend/static/js/priorities.js
+++ Frontend/static/js/priorities.js
@@ -59,25 +59,55 @@ $(function(){
 		// Show/Hide Valid Metatypes
 		if (index == 4){
 			// Magician, Mystic Adept, Technomancer
-			$('.magic-type').addClass('disabled') 
-			$('.type-magician').removeClass('disabled')
-			$('.type-mystic-adept').removeClass('disabled')
-			$('.type-technomancer').removeClass('disabled')
+			$('.magic-type').addClass('disabled');
+			$('.type-magician').removeClass('disabled');
+			$('.type-mystic-adept').removeClass('disabled');
+			$('.type-technomancer').removeClass('disabled');
+			$('.type-magician').attr('data-content', 'Magic 6, two Rating 5 Magical skills, 10 spells');
+			$('.type-mystic-adept').attr('data-content', 'Magic 6, two Rating 5 Magical skills, 10 spells');
+			$('.type-technomancer').attr('data-content', 'Resonance 6, two Rating 5 Resonance skills, 5 complex forms');
+			$('.type-adept').attr('data-content', '');
+			$('.type-aspected-magician').attr('data-content', '');
 		} else if (index == 3){
 			// Any
-			$('.magic-type').removeClass('disabled') 
+			$('.magic-type').removeClass('disabled');
+			$('.type-magician').attr('data-content', 'Magic 4, two Rating 4 Magical skills, 7 spells');
+			$('.type-mystic-adept').attr('data-content', 'Magic 4, two Rating 4 Magical skills, 7 spells');
+			$('.type-technomancer').attr('data-content', 'Resonance 4, two Rating 4 Resonance skills, 2 complex forms');
+			$('.type-adept').attr('data-content', 'Magic 6, one Rating 4 Active skill');
+			$('.type-aspected-magician').attr('data-content', 'Magic 5, one Rating 4 Magical skill group');
+			
 		} else if (index == 2){
 			// Any
-			$('.magic-type').removeClass('disabled') 
-		}  else if (index == 1){
+			$('.magic-type').removeClass('disabled');
+
+			$('.type-magician').attr('data-content', 'Magic 3, 5 spells');
+			$('.type-mystic-adept').attr('data-content', 'Magic 3, 5 spells');
+			$('.type-technomancer').attr('data-content', 'Resonance 3, 1 complex form');
+			$('.type-adept').attr('data-content', 'Magic 4, one Rating 2 Active skill');
+			$('.type-aspected-magician').attr('data-content', 'Magic 3, one Rating 2 Magical skill group');
+		} else if (index == 1){
 			// Adept+Aspected Magician
 			$('.magic-type').addClass('disabled') 
-			$('.magic-adept').removeClass('disabled') 
-			$('.magic-aspected-magician').removeClass('disabled') 
+			$('.type-adept').removeClass('disabled') 
+			$('.type-aspected-magician').removeClass('disabled') 
+
+
+			$('.type-magician').attr('data-content', '');
+			$('.type-mystic-adept').attr('data-content', '');
+			$('.type-technomancer').attr('data-content', '');
+			$('.type-adept').attr('data-content', 'Magic 2');
+			$('.type-aspected-magician').attr('data-content', 'Magic 2');
 		} else {
 			// At least they won't geek you first
-			$('.meta').addClass('disabled') 
-			$('.meta-human').removeClass('disabled') 
+			$('.magic-type').addClass('disabled') 
+			$('.magic-none').removeClass('disabled') 
+
+			$('.type-magician').attr('data-content', '');
+			$('.type-mystic-adept').attr('data-content', '');
+			$('.type-technomancer').attr('data-content', '');
+			$('.type-adept').attr('data-content', '');
+			$('.type-aspected-magician').attr('data-content', '');
 		}
 	});	
 
